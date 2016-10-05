49be19d9609187f8fa4a696d1f861d9468340532
Christian Auth <c.auth@syzygy.de>
2016-10-06 00:33:37 +0200

    style(model): uses consts where possible and adds some comments

--- test/model/entity/EntityCategoriesRepositorySpec.js
+++ test/model/entity/EntityCategoriesRepositorySpec.js
@@ -3,13 +3,17 @@
 /**
  * Requirements
  */
-let EntityCategoriesRepository = require(SOURCE_ROOT + '/model/entity/EntityCategoriesRepository.js').EntityCategoriesRepository;
-let baseRepositorySpec = require('../BaseRepositoryShared.js').spec;
+const EntityCategoriesRepository = require(SOURCE_ROOT + '/model/entity/EntityCategoriesRepository.js').EntityCategoriesRepository;
+const baseRepositorySpec = require(TEST_ROOT + '/model/BaseRepositoryShared.js').spec;
+const co = require('co');
 
 /**
  * Spec
  */
 describe(EntityCategoriesRepository.className, function()
 {
+    /**
+     * BaseRepository Tests
+     */
     baseRepositorySpec(EntityCategoriesRepository, 'model.entity/EntityCategoriesRepository');
 });
