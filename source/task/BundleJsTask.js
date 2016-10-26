8741449f99de66d5e982d3d2599d82d275e9912e
Christian Auth <c.auth@syzygy.de>
2016-10-26 20:48:44 +0200

    feat(task): adds BundleJsTask

---
+++ source/task/BundleJsTask.js
@@ -0,0 +1,307 @@
+'use strict';
+
+/**
+ * Requirements
+ * @ignore
+ */
+const BaseTask = require('./BaseTask.js').BaseTask;
+const FilesRepository = require('../model/file/FilesRepository.js').FilesRepository;
+const PathesConfiguration = require('../model/configuration/PathesConfiguration.js').PathesConfiguration;
+const SitesRepository = require('../model/site/SitesRepository.js').SitesRepository;
+const ContentType = require('../model/ContentType.js');
+const Site = require('../model/site/Site.js').Site;
+const CliLogger = require('../cli/CliLogger.js').CliLogger;
+const assertParameter = require('../utils/assert.js').assertParameter;
+const pathes = require('../utils/pathes.js');
+const Builder = require('systemjs-builder');
+const through2 = require('through2');
+const VinylFile = require('vinyl');
+const difference = require('lodash.difference');
+const co = require('co');
+const babel = require('babel-core');
+const PATH_SEPERATOR = require('path').sep;
+const fs = require('co-fs-extra');
+
+
+/**
+ * @memberOf task
+ */
+class BundleJsTask extends BaseTask
+{
+    /**
+     *
+     */
+    constructor(cliLogger, filesRepository, sitesRepository, pathesConfiguration)
+    {
+        super(cliLogger);
+
+        //Check params
+        assertParameter(this, 'filesRepository', filesRepository, true, FilesRepository);
+        assertParameter(this, 'sitesRepository', sitesRepository, true, SitesRepository);
+        assertParameter(this, 'pathesConfiguration', pathesConfiguration, true, PathesConfiguration);
+
+        // Assign options
+        this._filesRepository = filesRepository;
+        this._sitesRepository = sitesRepository;
+        this._pathesConfiguration = pathesConfiguration;
+        this._defaultGroup = 'common';
+    }
+
+
+    /**
+     * @inheritDocs
+     */
+    static get injections()
+    {
+        return { 'parameters': [CliLogger, FilesRepository, SitesRepository, PathesConfiguration] };
+    }
+
+
+    /**
+     * @inheritDocs
+     */
+    static get className()
+    {
+        return 'task/BundleJsTask';
+    }
+
+
+    /**
+     * @protected
+     * @returns {Promise<Array>}
+     */
+    generateConfiguration(buildConfiguration, parameters)
+    {
+        const scope = this;
+        const promise = co(function *()
+        {
+            // Prepare
+            const query = parameters ? parameters.query || '*' : '*';
+
+            // Start
+            const work = scope._cliLogger.section('Generating bundle configuration for <' + query + '>');
+
+            // Get Sites
+            let sites = [];
+            if (query !== '*')
+            {
+                const site = yield scope._sitesRepository.findBy(Site.ANY, query);
+                sites.push(site);
+            }
+            else
+            {
+                sites = yield scope._sitesRepository.getItems();
+            }
+
+            // Get bundles
+            const result = [];
+            for (const site of sites)
+            {
+                // Get js sources
+                const filter = function(file)
+                {
+                    return file.contentType === ContentType.JS;
+                };
+                const sourceFiles = yield scope._filesRepository.getBySiteGrouped(site, filter, 'groups.js', scope._defaultGroup);
+
+                // Collect all modules
+                const all = [];
+                for (const group in sourceFiles)
+                {
+                    for (const file of sourceFiles[group])
+                    {
+                        all.push(file.filename.replace(scope._pathesConfiguration.sites + PATH_SEPERATOR, ''));
+                    }
+                }
+
+                // Create bundles
+                const bundles = {};
+                for (const group in sourceFiles)
+                {
+                    const groupWork = scope._cliLogger.work('Genrating bundle config for <' + site.name + '> / <' + group + '>');
+                    const bundle =
+                    {
+                        filename : site.name.toLowerCase() + PATH_SEPERATOR + group.toLowerCase() + '.js',
+                        prepend: [],
+                        append: [],
+                        include: [],
+                        exclude: []
+                    };
+
+                    // Add include
+                    for (const file of sourceFiles[group])
+                    {
+                        const url = file.filename.replace(scope._pathesConfiguration.sites + PATH_SEPERATOR, '');
+                        bundle.include.push(url);
+                    }
+
+                    // Generate exclude
+                    bundle.exclude = difference(all, bundle.include);
+
+                    // Add jspm when default category
+                    if (group === scope._defaultGroup)
+                    {
+                        bundle.prepend.push(pathes.concat(scope._pathesConfiguration.entoj, 'jspm_packages', 'system-polyfills.js'));
+                        bundle.prepend.push(pathes.concat(scope._pathesConfiguration.entoj, 'jspm_packages', 'system.src.js'));
+                        bundle.prepend.push(pathes.concat(scope._pathesConfiguration.entoj, 'jspm.js'));
+                    }
+
+                    // Add bundle
+                    bundles[group] = bundle;
+                    scope._cliLogger.end(groupWork);
+                }
+
+                result.push(bundles);
+            }
+
+            // End
+            scope._cliLogger.end(work);
+            return result;
+        });
+        return promise;
+    }
+
+
+    /**
+     * @protected
+     * @returns {Promise<Array>}
+     */
+    compileBundles(bundles, buildConfiguration, parameters)
+    {
+        const scope = this;
+        const promise = co(function *()
+        {
+            // Prepare builder
+            const builderConfig =
+            {
+                transpiler: 'babel',
+                babelOptions:
+                {
+                    'optional':
+                    [
+                        'runtime'
+                    ]
+                },
+                paths:
+                {
+                    'jspm_packages/*': scope._pathesConfiguration.entoj + '/jspm_packages/*'
+                }
+            }
+            const sites = yield scope._sitesRepository.getItems();
+            for (const site of sites)
+            {
+                builderConfig.paths[site.name.urlify() + '/*'] = scope._pathesConfiguration.sites + '/' + site.name.urlify() + '/*'
+            }
+            const builder = new Builder(scope._pathesConfiguration.entoj, scope._pathesConfiguration.entoj + '/jspm.js');
+            builder.config(builderConfig);
+
+            // Prepare bundler config
+            const bundlerConfig =
+            {
+                runtime: false,
+                minify: false,
+                sourceMaps: false,
+                lowResSourceMaps: false,
+                fetch: function (load, fetch)
+                {
+                    const promise = co(function *()
+                    {
+                        const filename = yield scope._pathesConfiguration.shorten(load.name);
+                        const work = scope._cliLogger.work(filename);
+                        const result = yield fetch(load);
+                        scope._cliLogger.end(work, false, 'Added ' + filename);
+                        return result;
+                    });
+                    return promise;
+                }
+            };
+
+            // Build bundles
+            const result = [];
+            for (const name in bundles)
+            {
+                const section = scope._cliLogger.section('Creating bundle <' + bundles[name].filename + '>');
+
+                // Get bundle arithmetic
+                const bundle = bundles[name];
+                let modules = bundle.include.join(' + ');
+                // Remove excludes when not default group
+                if (name !== scope._defaultGroup && bundle.exclude.length)
+                {
+                    modules+= ' - ' + bundle.exclude.join(' - ');
+                }
+
+                // Build bundle
+                const bundled = yield builder.bundle(modules, bundlerConfig);
+
+                // Create file
+                let contents = '';
+
+                // Prepend files
+                for (const filename of bundle.prepend)
+                {
+                    const work = scope._cliLogger.work(filename);
+                    const fileContent = yield fs.readFile(filename, { encoding: 'utf8' });
+                    contents+= fileContent;
+                    scope._cliLogger.end(work, false, 'Prepended ' + filename);
+                }
+
+                // Bundle
+                contents+= bundled.source;
+                const file = new VinylFile(
+                    {
+                        path: bundle.filename,
+                        contents: new Buffer(contents)
+                    });
+                result.push(file);
+
+                // Done
+                scope._cliLogger.end(section);
+            }
+
+            return result;
+        })
+        .catch((e) => console.log(e) );
+        return promise;
+    }
+
+
+    /**
+     * @returns {Stream}
+     */
+    stream(stream, buildConfiguration, parameters)
+    {
+        let resultStream = stream;
+        if (!resultStream)
+        {
+            resultStream = through2(
+            {
+                objectMode: true
+            });
+            const scope = this;
+            const promise = co(function *()
+            {
+                const siteBundles = yield scope.generateConfiguration(buildConfiguration, parameters);
+                const work = scope._cliLogger.section('Bundling js files');
+                for (const siteBundle of siteBundles)
+                {
+                    const siteFiles = yield scope.compileBundles(siteBundle, buildConfiguration, parameters);
+                    for (const siteFile of siteFiles)
+                    {
+                        resultStream.write(siteFile);
+                    }
+                }
+                resultStream.end();
+                scope._cliLogger.end(work);
+            });
+        }
+        return resultStream;
+    }
+}
+
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports.BundleJsTask = BundleJsTask;
---
+++ test/task/BundleJsTaskSpec.js
@@ -0,0 +1,180 @@
+"use strict";
+
+/**
+ * Requirements
+ */
+const BundleJsTask = require(SOURCE_ROOT + '/task/BundleJsTask.js').BundleJsTask;
+const FilesRepository = require(SOURCE_ROOT + '/model/file/FilesRepository.js').FilesRepository;
+const SitesRepository = require(SOURCE_ROOT + '/model/site/SitesRepository.js').SitesRepository;
+const PathesConfiguration = require(SOURCE_ROOT + '/model/configuration/PathesConfiguration.js').PathesConfiguration;
+const CliLogger = require(SOURCE_ROOT + '/cli/CliLogger.js').CliLogger;
+const baseTaskSpec = require(TEST_ROOT + '/task/BaseTaskShared.js');
+const create = require(SOURCE_ROOT + '/utils/objects.js').create;
+const compact = require(FIXTURES_ROOT + '/Application/Compact.js');
+const pathes = require(SOURCE_ROOT + '/utils/pathes.js');
+const co = require('co');
+const VinylFile = require('vinyl');
+const fs = require('fs-extra');
+const sinon = require('sinon');
+
+
+
+/**
+ * Spec
+ */
+describe(BundleJsTask.className, function()
+{
+    /**
+     * BaseTask Test
+     */
+    baseTaskSpec(BundleJsTask, 'task/BundleJsTask', prepareParameters);
+
+    /**
+     */
+    function prepareParameters(parameters)
+    {
+        parameters.unshift(fixtures.pathesConfiguration);
+        parameters.unshift(fixtures.sitesRepository);
+        parameters.unshift(fixtures.filesRepository);
+        parameters.unshift(fixtures.cliLogger);
+        return parameters;
+    };
+
+
+    /**
+     */
+    const createTestee = function()
+    {
+        let parameters = Array.from(arguments);
+        if (prepareParameters)
+        {
+            parameters = prepareParameters(parameters);
+        }
+        return create(BundleJsTask, parameters);
+    };
+
+
+    /**
+     * BundleJsTask Test
+     */
+    beforeEach(function()
+    {
+        fixtures = compact.createFixture();
+        fixtures.cliLogger = fixtures.context.di.create(CliLogger);
+        fixtures.cliLogger.muted = true;
+        fixtures.filesRepository = fixtures.context.di.create(FilesRepository);
+        fixtures.sitesRepository = fixtures.context.di.create(SitesRepository);
+        fixtures.pathesConfiguration = fixtures.context.di.create(PathesConfiguration);
+        fixtures.path = pathes.concat(FIXTURES_ROOT, '/Tasks/BundleJsTask');
+        fs.emptyDirSync(fixtures.path);
+    });
+
+
+    describe('#generateConfiguration()', function()
+    {
+        it('should return a promise', function()
+        {
+            const testee = createTestee();
+            const promise = testee.generateConfiguration();
+            expect(promise).to.be.instanceof(Promise);
+            return promise;
+        });
+
+        it('should resolve to an array containing bundle configs for each site', function()
+        {
+            const promise = co(function *()
+            {
+                const testee = createTestee();
+                const bundles = yield testee.generateConfiguration();
+                expect(bundles).to.be.instanceof(Array);
+                expect(bundles).to.have.length(2);
+            });
+            return promise;
+        });
+
+        it('should generate bundle configs that contains all groups', function()
+        {
+            const promise = co(function *()
+            {
+                const testee = createTestee();
+                const bundles = yield testee.generateConfiguration();
+                const baseBundle = bundles[0];
+                const extendedBundle = bundles[1];
+                expect(baseBundle).to.have.contain.key('common');
+                expect(baseBundle).to.have.contain.key('core');
+                expect(extendedBundle).to.have.contain.key('common');
+                expect(extendedBundle).to.have.contain.key('extended');
+            });
+            return promise;
+        });
+
+        it('should generate bundle configs that contains all in/excluded files for a group', function()
+        {
+            const promise = co(function *()
+            {
+                const testee = createTestee();
+                const bundles = yield testee.generateConfiguration();
+                const baseBundle = bundles[0];
+                expect(baseBundle.common.include).to.have.length(2);
+                expect(baseBundle.common.include).to.contain('base/common/js/bootstrap.js');
+                expect(baseBundle.common.include).to.contain('base/common/js/component.js');
+                expect(baseBundle.common.exclude).to.have.length(1);
+                expect(baseBundle.common.exclude).to.contain('base/modules/m001-gallery/js/m001-gallery.js');
+                expect(baseBundle.core.include).to.have.length(1);
+                expect(baseBundle.core.include).to.contain('base/modules/m001-gallery/js/m001-gallery.js');
+                expect(baseBundle.core.exclude).to.have.length(2);
+                expect(baseBundle.core.exclude).to.contain('base/common/js/bootstrap.js');
+                expect(baseBundle.core.exclude).to.contain('base/common/js/component.js');
+            });
+            return promise;
+        });
+
+        it('should generate bundle configs for extended sites that contains all in/excluded files for a group', function()
+        {
+            const promise = co(function *()
+            {
+                const testee = createTestee();
+                const bundles = yield testee.generateConfiguration();
+                const extendedBundle = bundles[1];
+                expect(extendedBundle.common.include).to.have.length(2);
+                expect(extendedBundle.common.include).to.contain('base/common/js/bootstrap.js');
+                expect(extendedBundle.common.include).to.contain('base/common/js/component.js');
+                expect(extendedBundle.common.exclude).to.have.length(2);
+                expect(extendedBundle.common.exclude).to.contain('base/modules/m001-gallery/js/m001-gallery.js');
+                expect(extendedBundle.common.exclude).to.contain('extended/modules/m001-gallery/js/m001-gallery.js');
+                expect(extendedBundle.extended.include).to.have.length(2);
+                expect(extendedBundle.extended.include).to.contain('base/modules/m001-gallery/js/m001-gallery.js');
+                expect(extendedBundle.extended.include).to.contain('extended/modules/m001-gallery/js/m001-gallery.js');
+                expect(extendedBundle.extended.exclude).to.have.length(2);
+                expect(extendedBundle.extended.exclude).to.contain('base/common/js/bootstrap.js');
+                expect(extendedBundle.extended.exclude).to.contain('base/common/js/component.js');
+            });
+            return promise;
+        });
+    });
+
+    describe('#compileBundle()', function()
+    {
+        it('should create a file for a configured bundle group', function()
+        {
+            const promise = co(function *()
+            {
+                const bundleConfiguration =
+                {
+                    common:
+                    {
+                        filename: 'base/common.js',
+                        prepend: [],
+                        append: [],
+                        include: [ 'base/common/js/bootstrap.js', 'base/common/js/component.js' ],
+                        exclude: [ 'base/modules/m001-gallery/js/m001-gallery.js' ]
+                    }
+                }
+                const testee = createTestee();
+                const files = yield testee.compileBundles(bundleConfiguration);
+                //console.log(files[0].contents.toString());
+            });
+            return promise;
+        });
+    });
+});
