966ed85f0cad677ace52a876ac2020d9ed48f2f1
Christian Auth <c.auth@syzygy.de>
2016-10-27 12:25:41 +0200

    feat(task): adds ReadFilesTask

---
+++ source/task/ReadFilesTask.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * Requirements
+ * @ignore
+ */
+const BaseTask = require('./BaseTask.js').BaseTask;
+const gulp = require('gulp');
+
+
+/**
+ * @memberOf task
+ */
+class ReadFilesTask extends BaseTask
+{
+    /**
+     * @inheritDocs
+     */
+    static get className()
+    {
+        return 'task/ReadFilesTask';
+    }
+
+
+    /**
+     * @returns {Stream}
+     */
+    stream(stream, buildConfiguration, parameters)
+    {
+        if (!parameters || (!parameters.path && !parameters.readPath))
+        {
+            return super.stream(stream, buildConfiguration, parameters);
+        }
+
+        const path = parameters.path || parameters.readPath;
+        const work = this._cliLogger.info('Reading files from <' + path + '>');
+        return gulp.src(path);
+    }
+}
+
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports.ReadFilesTask = ReadFilesTask;
---
+++ test/task/ReadFilesTaskSpec.js
@@ -0,0 +1,65 @@
+"use strict";
+
+/**
+ * Requirements
+ */
+const ReadFilesTask = require(SOURCE_ROOT + '/task/ReadFilesTask.js').ReadFilesTask;
+const CliLogger = require(SOURCE_ROOT + '/cli/CliLogger.js').CliLogger;
+const baseTaskSpec = require(TEST_ROOT + '/task/BaseTaskShared.js');
+const pathes = require(SOURCE_ROOT + '/utils/pathes.js');
+const co = require('co');
+const through2 = require('through2');
+const VinylFile = require('vinyl');
+const fs = require('fs-extra');
+const sinon = require('sinon');
+
+
+
+/**
+ * Spec
+ */
+describe(ReadFilesTask.className, function()
+{
+    /**
+     * BaseTask Test
+     */
+    baseTaskSpec(ReadFilesTask, 'task/ReadFilesTask', prepareParameters);
+
+    /**
+     */
+    function prepareParameters(parameters)
+    {
+        parameters.unshift(fixtures.cliLogger);
+        return parameters;
+    };
+
+
+    /**
+     * WriteFilesTask Test
+     */
+    beforeEach(function()
+    {
+        fixtures.cliLogger = new CliLogger();
+        fixtures.cliLogger.muted = true;
+        fixtures.path = pathes.concat(FIXTURES_ROOT, '/Nunjucks/**/*.json');
+    });
+
+
+    describe('#stream()', function()
+    {
+        it('should read files from the filesystem', function()
+        {
+            const promise = co(function *()
+            {
+                const testee = new ReadFilesTask(fixtures.cliLogger);
+                const data = yield baseTaskSpec.readStream(testee.stream(undefined, undefined, { path: fixtures.path }));
+                expect(data).to.have.length(3);
+                for (const file of data)
+                {
+                    expect(file.path).to.endWith('.json');
+                }
+            });
+            return promise;
+        });
+    });
+});
