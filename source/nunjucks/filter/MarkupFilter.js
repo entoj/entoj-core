91d1ddc7b31db8bfc3df76a67559d41b3c3bf898
Christian Auth <c.auth@syzygy.de>
2017-04-12 18:25:34 +0200

    fix(nunjucks): add support for plain markup

--- source/nunjucks/filter/MarkupFilter.js
+++ source/nunjucks/filter/MarkupFilter.js
@@ -38,6 +38,10 @@ class MarkupFilter extends BaseFilter
     {
         return function (value, style)
         {
+            if (style == 'plain' || style == 'tkPlain')
+            {
+                return value;
+            }
             if (value && value.indexOf('<') > -1)
             {
                 return value;
